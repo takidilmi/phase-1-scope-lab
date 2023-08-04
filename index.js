11076ee9a550dbbbf9840af4e151cad028fa103a
takidilmi <angeldilmi@gmail.com>
2023-08-04 15:36:58 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+    customerName = customerName.toUpperCase();
+  } else {
+    console.log("customerName is not a string or not defined!");
+  }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'IDK';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'NoOne';    
+}
+
